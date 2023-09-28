b2c41c040a31d11b48f828c48164eb4bdc0e75ef
samuel <olaoluwasamuel119@gmail.com>
2023-09-28 16:30:11 -0400

    new push

--- src/data/data.js
+++ src/data/data.js
@@ -10,9 +10,33 @@ export const data = [
     github: 'https://github.com/Temitech001/MiddleweightMainevent',
     live: 'https://middleweight-mainevent.vercel.app/',
   },
-
   {
     id: 2,
+    name: 'A City Weather App',
+    image:
+      '  https://res.cloudinary.com/dutvrzezk/image/upload/v1695932521/2023-09-28_16-19-08-71_ulfkxg.png',
+    github: 'https://github.com/Temitech001/A-weather-app',
+    live: 'https://a-weather-app-nine.vercel.app/',
+  },
+  {
+    id: 3,
+    name: 'Timer',
+    image:
+      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695631963/2023-09-25_04-48-39-75_pfcbih.png',
+    github: 'https://github.com/Temitech001/stopwatch',
+    live: 'https://stopwatch-flax.vercel.app/',
+  },
+
+  {
+    id: 4,
+    name: 'A Todo do list App',
+    image:
+      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695046868/todolist_llodtm.png',
+    github: 'https://github.com/Temitech001/todolist',
+    live: 'https://todolistproject-red.vercel.app/',
+  },
+  {
+    id: 5,
     name: 'A Food Resturant',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695633720/2023-09-25_05-21-01-93_nwvxil.png',
@@ -21,7 +45,7 @@ export const data = [
   },
 
   {
-    id: 3,
+    id: 6,
     name: 'Bankist Web App',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695630503/2023-09-25_04-25-23-46_qy49jo.png',
@@ -30,15 +54,7 @@ export const data = [
   },
 
   {
-    id: 4,
-    name: 'Timer',
-    image:
-      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695631963/2023-09-25_04-48-39-75_pfcbih.png',
-    github: 'https://github.com/Temitech001/stopwatch',
-    live: 'https://stopwatch-flax.vercel.app/',
-  },
-  {
-    id: 5,
+    id: 7,
     name: 'Pizza Order Website',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695630503/2023-09-25_04-16-03-36_rmqrge.png',
@@ -46,7 +62,7 @@ export const data = [
     live: 'https://pizza-eight-eta.vercel.app/',
   },
   {
-    id: 6,
+    id: 8,
     name: 'A shopping Cart Website',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695046868/shopin_ysb948.png',
@@ -54,7 +70,7 @@ export const data = [
     live: 'https://shopping-zen.vercel.app/',
   },
   {
-    id: 7,
+    id: 9,
     name: 'Suprise Me',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695911670/2023-09-28_10-33-52-08_dwaduk.png',
@@ -63,44 +79,36 @@ export const data = [
   },
 
   {
-    id: 8,
-    name: 'A Todo do list App',
-    image:
-      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695046868/todolist_llodtm.png',
-    github: 'https://github.com/Temitech001/todolist',
-    live: 'https://todolistproject-red.vercel.app/',
-  },
-  {
-    id: 9,
+    id: 10,
     name: 'My Porfolio Website',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695630502/2023-09-25_04-17-14-37_kek6l5.png',
     github: 'https://github.com/Temitech001/main2portfolio',
     live: 'https://mymainportfolio-flax.vercel.app/',
   },
-
-  {
-    id: 10,
-    name: 'Flash-Card',
-    image:
-      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695910405/2023-09-28_10-09-04-51_w1fmsq.png',
-    github: 'https://github.com/Temitech001/flashCard',
-    live: 'https://flash-card-pr.vercel.app/',
-  },
   {
     id: 11,
-    name: 'A pig Game',
-    image:
-      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695631964/2023-09-25_04-50-01-55_vz3xuf.png',
-    github: 'https://github.com/Temitech001/piggame',
-    live: 'https://piggame-smoky.vercel.app/',
-  },
-  {
-    id: 12,
     name: 'A Walk out Tracker',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695631964/2023-09-18_10-10-57-31_ntsacf.png',
     github: 'https://github.com/Temitech001/map',
     live: 'https://walkouttracker.vercel.app/',
   },
+
+  {
+    id: 12,
+    name: 'A pig Game',
+    image:
+      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695631964/2023-09-25_04-50-01-55_vz3xuf.png',
+    github: 'https://github.com/Temitech001/piggame',
+    live: 'https://piggame-smoky.vercel.app/',
+  },
+  // {
+  //   id: 13,
+  //   name: 'Flash-Card',
+  //   image:
+  //     'https://res.cloudinary.com/dutvrzezk/image/upload/v1695910405/2023-09-28_10-09-04-51_w1fmsq.png',
+  //   github: 'https://github.com/Temitech001/flashCard',
+  //   live: 'https://flash-card-pr.vercel.app/',
+  // },
 ];
