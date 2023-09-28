d0a68f405e1774bd393224d4787c18ca6f8de996
samuel <olaoluwasamuel119@gmail.com>
2023-09-28 07:09:57 -0400

    new one

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,16 +8,16 @@ const About = () => {
     >
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-          <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+          <div className="sm:text-right pb-8 pl-4 text-center mt-4">
+            <p className="  text-4xl font-bold inline border-b-4 border-pink-600  ">
               About
             </p>
           </div>
           <div></div>
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-          <div className=" sm:text-right text-4xl font-bold">
-            <p>
+          <div className=" sm:text-right text-sm lg:text-4xl font-bold">
+            <p className="fo">
               Hi. I'm Monsuru Samuel, nice to meet you. Please take a look
               around.
             </p>
@@ -28,7 +28,7 @@ const About = () => {
               alt="my pics"
             />
           </div>
-          <div>
+          <div className="text-sm px-3">
             <p>
               i passionate web developer with a knack for creating visually
               stunning designs that captivate audiences. I believe that great
@@ -41,7 +41,7 @@ const About = () => {
                 boundaries to deliver seamless experiences and optimize websites
                 for better search engine visibility.
               </p>
-              <p>
+              <p className="bg-slate-100">
                 Beyond the realm of code and design, I find joy in the simple
                 pleasures of life. Music is a passion of mine, serving as a
                 constant source of inspiration and motivation throughout my
--- src/components/Couresel.jsx
+++ src/components/Couresel.jsx
@@ -16,9 +16,11 @@ function Couresel() {
   return (
     <div
       id="project"
-      className=" bg-slate-100 flex  mt-[130px] flex-col  items-center justify-center gap-10 h-screen lg:hidden mx-[50px] "
+      className="  flex bg-slate-100 mt-[120px] flex-col  items-center justify-center gap-10 h-screen lg:hidden mx-[50px] "
     >
-      <h2 className="flex justify-center  text-2xl font-semibold ">Project</h2>
+      <h2 className="bg-slate-100 flex justify-center  text-2xl font-semibold ">
+        Project
+      </h2>
       <div className="flex justify-center items-center gap-5">
         <button
           onClick={handlePrevious}
