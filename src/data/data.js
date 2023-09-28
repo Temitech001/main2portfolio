f17f34021b6e441056c9cb49cff0e52c57ae88c2
samuel <olaoluwasamuel119@gmail.com>
2023-09-28 09:58:10 -0400

    new commit

--- src/data/data.js
+++ src/data/data.js
@@ -13,56 +13,66 @@ export const data = [
 
   {
     id: 2,
-    name: 'A Todo do list App',
-    image:
-      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695046868/todolist_llodtm.png',
-    github: 'https://github.com/Temitech001/todolist',
-    live: 'https://todolistproject-red.vercel.app/',
-  },
-
-  {
-    id: 3,
-    name: 'A shopping Cart Website',
-    image:
-      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695046868/shopin_ysb948.png',
-    github: 'https://github.com/Temitech001/shopping',
-    live: 'https://shopping-zen.vercel.app/',
-  },
-  {
-    id: 4,
     name: 'A Food Resturant',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695633720/2023-09-25_05-21-01-93_nwvxil.png',
     github: 'https://github.com/Temitech001/food',
     live: 'https://food-resturant-six.vercel.app/',
   },
+
   {
-    id: 5,
+    id: 3,
     name: 'Bankist Web App',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695630503/2023-09-25_04-25-23-46_qy49jo.png',
     github: 'https://github.com/Temitech001/bankwebapp',
     live: 'https://bankistapp-delta.vercel.app/',
   },
+
   {
-    id: 6,
-    name: 'My Porfolio Website',
+    id: 4,
+    name: 'Timer',
     image:
-      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695630502/2023-09-25_04-17-14-37_kek6l5.png',
-    github: 'https://github.com/Temitech001/form-validation',
-    live: 'https://mymainportfolio-flax.vercel.app/',
+      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695631963/2023-09-25_04-48-39-75_pfcbih.png',
+    github: 'https://github.com/Temitech001/stopwatch',
+    live: 'https://stopwatch-flax.vercel.app/',
   },
   {
-    id: 7,
+    id: 5,
     name: 'Pizza Order Website',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695046868/forvalida_kfk51r.png',
     github: 'https://github.com/Temitech001/pizza',
     live: 'https://pizza-eight-eta.vercel.app/',
   },
+  {
+    id: 6,
+    name: 'A shopping Cart Website',
+    image:
+      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695046868/shopin_ysb948.png',
+    github: 'https://github.com/Temitech001/shopping',
+    live: 'https://shopping-zen.vercel.app/',
+  },
 
   {
+    id: 7,
+    name: 'A Todo do list App',
+    image:
+      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695046868/todolist_llodtm.png',
+    github: 'https://github.com/Temitech001/todolist',
+    live: 'https://todolistproject-red.vercel.app/',
+  },
+  {
     id: 8,
+    name: 'My Porfolio Website',
+    image:
+      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695630502/2023-09-25_04-17-14-37_kek6l5.png',
+    github: 'https://github.com/Temitech001/form-validation',
+    live: 'https://mymainportfolio-flax.vercel.app/',
+  },
+
+  {
+    id: 9,
     name: 'Form Validation',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695046868/forvalida_kfk51r.png',
@@ -70,7 +80,7 @@ export const data = [
     live: 'https://form-validation-psi-ten.vercel.app/',
   },
   {
-    id: 9,
+    id: 10,
     name: 'A pig Game',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695631964/2023-09-25_04-50-01-55_vz3xuf.png',
@@ -78,20 +88,12 @@ export const data = [
     live: 'https://piggame-smoky.vercel.app/',
   },
   {
-    id: 10,
+    id: 11,
     name: 'A Walk out Tracker',
     image:
       'https://res.cloudinary.com/dutvrzezk/image/upload/v1695631964/2023-09-18_10-10-57-31_ntsacf.png',
     github: 'https://github.com/Temitech001/map',
     live: 'https://walkouttracker.vercel.app/',
-  },
-  {
-    id: 11,
-    name: 'Timer',
-    image:
-      'https://res.cloudinary.com/dutvrzezk/image/upload/v1695631963/2023-09-25_04-48-39-75_pfcbih.png',
-    github: 'https://github.com/Temitech001/stopwatch',
-    live: 'https://stopwatch-flax.vercel.app/',
   },
 
   {
